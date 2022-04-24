f3e41d5530a4eb01733045935bb86689e63cdffa
kukovisuals <clubhousenet@live.com>
2022-04-24 18:49:13 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import data from './quotes.json'
 
 function App() {
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+  const quoteData = data.quotes
+   
+  const randomQuote = (e) => {
+    const len = quoteData.length
+    const randomIndex = Math.round( Math.random() * (len - 1 ))
+    setQuote(quoteData[randomIndex].quote)
+    setAuthor(quoteData[randomIndex].author)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div id="quote-box">
+        <p id="text">{quote} </p>
+        <p id="author"> {author}</p>
+        <button id="new-quote" onClick={randomQuote}>New Quote</button>
+        <a id="tweet-quote"
+        href="https://twitter.com/intent/tweet"
         >
-          Learn React
+         Tweet
         </a>
-      </header>
+      </div>
     </div>
   );
 }
